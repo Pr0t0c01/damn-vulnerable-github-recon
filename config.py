07bfea5619cc229ed7521fc9a4856f4b2ffa8ade
Shir0E <117108743+Pr0t0c01@users.noreply.github.com>
2025-06-06 11:44:29 +0630

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+# Fake credentials for testing
+DB_USER = "admin"
+DB_PASSWORD = "P@ssw0rd1234"
+DB_HOST = "localhost"
+DB_NAME = "test_db"
+
+GITHUB_TOKEN = "ghp_abcd1234EXAMPLETOKEN5678xyz"
+SLACK_WEBHOOK = "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX"
+JWT_SECRET = "supersecretjwtkey_example"
